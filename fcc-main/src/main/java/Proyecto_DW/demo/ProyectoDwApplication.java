37c60858aca00b485bf658dc660c3fead50021ca
AaronO <4ar0n0rt1zfu3nt3s@gmail.com>
2026-03-09 19:21:16 -0600

    .

--- fcc-main/src/main/java/Proyecto_DW/demo/ProyectoDwApplication.java
+++ fcc-main/src/main/java/Proyecto_DW/demo/ProyectoDwApplication.java
@@ -3,8 +3,10 @@ package Proyecto_DW.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-@SpringBootApplication
+//@Configuration
+//@ComponentScan(basePackages = "Proyecto_DW")
+class VehicleFactoryConfig {}
+@SpringBootApplication 
 public class ProyectoDwApplication {
 
     public static void main(String[] args) {
