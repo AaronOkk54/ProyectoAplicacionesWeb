cb8d002ca9055eebcb78c14dd792ad77472c14a8
josue alfaro <josuealfaroc28@gmail.com>
2026-04-21 14:45:45 -0600

    Dependencias Google

--- fcc-main/src/main/java/Proyecto_DW/service/CorreoService.java
+++ fcc-main/src/main/java/Proyecto_DW/service/CorreoService.java
@@ -4,10 +4,36 @@
  */
 package Proyecto_DW.service;
 
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
 /**
  *
  * @author Alfaro
  */
+@Service
 public class CorreoService {
-    
+
+    private final JavaMailSender mailSender;
+
+    public CorreoService(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void enviarCorreoHtml(String para,
+            String asunto,
+            String contenido) throws MessagingException {
+
+        MimeMessage mensaje = mailSender.createMimeMessage();
+        MimeMessageHelper correo = new MimeMessageHelper(mensaje, true);
+
+        correo.setTo(para);
+        correo.setSubject(asunto);
+        correo.setText(contenido, true);
+        mailSender.send(mensaje);
+    }
 }
+
